d63f633d0cf0b2e5dbdcf1145e371978776fbd81
AlvaroCode <alvlopsua@gmail.com>
2023-01-22 12:41:03 +0100

    Branch 6

---
+++ SRC/index.js
@@ -0,0 +1,16 @@
+const fn = () => {
+    const miString = "soy un string!"
+    let b = miString + "muy feliz"
+    console.log(b)
+    console.log('hola mundo');
+    
+    
+}
+
+fn()
+
+const clickeado = () => {
+    let a = "chanchito feliz"
+
+    console.log(a)
+}
